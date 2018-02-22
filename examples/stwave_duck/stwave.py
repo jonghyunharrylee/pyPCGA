a2172edd20c5f33b2f56b591253e9cc5bcad7d68
Harry Lee <jonghyun@stanford.edu>
2018-02-22 08:22:16 -0800

    updated stwave.py to use stwave_utils

--- examples/stwave_duck/stwave.py
+++ examples/stwave_duck/stwave.py
@@ -1,5 +1,11 @@
+import datetime as dt
 import os
+import sys
+from stwave_utils.stwave_forward_problem import STWaveProblem
+from stwave_utils.run_stwave import read_speed
+from multiprocessing import Pool
 import numpy as np
+
 from shutil import copy2, rmtree
 from subprocess import call, check_output
 from time import time
@@ -17,7 +23,9 @@ class Model:
         self.homedir = os.path.abspath('./')
         self.inputdir = os.path.abspath(os.path.join(self.homedir,"./input_files"))
         self.deletedir = True
-        self.ncores = 1
+
+        from psutil import cpu_count  # physcial cpu counts
+        self.ncores = cpu_count(logical=False)
 
         if params is not None: 
             if 'deletedir' in params:
@@ -29,6 +37,23 @@ class Model:
                 self.inputdir = params['inputdir']
             if 'ncores' in params:
                 self.ncores = params['ncores']
+
+        self.nx = params['nx']
+        self.ny = params['ny']
+        self.Lx = params['Lx']
+        self.Ly = params['Ly']
+        self.x0 = 0. if 'x0' not in params else params['x0']
+        self.y0 = 0. if 'y0' not in params else params['y0']
+        self.t1 = params['t1']
+        self.t2 = params['t2']
+        # note that outputdir is not used for simulation; force outputdir in ./simul/simul0000
+        self.outputdir = None if 'outputdir' not in params else params['outputdir']
+        self.parallel = False if 'parallel' not in params else params['parallel']
+        self.offline_dataloc = None if 'offline_dataloc' not in params else params['offline_dataloc']
+        self.offline_dataname = None if 'offline_dataname' not in params else params['offline_dataname']
+        self.true_elev_xyz_file = None if 'true_elev_xyz_file' not in params else params['true_elev_xyz_file']
+        self.frfdataloc = None if 'frfdataloc' not in params else params['frfdataloc']
+        self.chlDataLoc = None if 'chlDataLoc' not in params else params['chlDataLoc']
 
     def create_dir(self,idx=None):
         
@@ -47,209 +72,103 @@ class Model:
         
         return mydir
 
-    def write_input(self,bathy,idx=None):
-        '''
-            create directory, copy relevant files and write input with bathymetry
-        '''
-        if idx is None:
-            mydir = self.create_dir()
+    def cleanup(outputdir=None):
+        """
+        Removes outputdir if specified. Otherwise removes all output files
+        in the current working directory.
+        """
+        import shutil
+        import glob
+        log = "dummy.log"
+        if os.path.exists(log):
+            os.remove(log)
+        if outputdir is not None and outputdir != os.getcwd():
+            if os.path.exists(outputdir):
+                shutil.rmtree(outputdir)
         else:
-            mydir = self.create_dir(idx)
+            filelist = glob.glob("*.out")
+            filelist += glob.glob("*.sim")
+            filelist += glob.glob("*.dep")
+            filelist += glob.glob("*.0000")
+            filelist += glob.glob("*.eng")
+            filelist += glob.glob("*.dat")
+            for file in filelist:
+                os.remove(file)
 
-        file = open(os.path.join(mydir,"stwave_out.dep"),"wb")
-        file.write(b"#STWAVE_SPATIAL_DATASET\n")
-        # DATA DIMENSION
-        file.write(b"&DataDims\n")
-        file.write(b"   NI = 110,\n")
-        file.write(b"   NJ = 83,\n");
-        file.write(b"   DataType = 0,\n");
-        file.write(b"   NumRecs = 1,\n");
-        file.write(b"   DX = 5.0,\n");
-        file.write(b"   DY = 5.0,\n");
-        file.write(b"   GridName = \"stwave_out\",\n")
-        file.write(b"   NumFlds = 1,\n");
-        file.write(b"/\n");
-        # Dataset information
-        file.write(b"&Dataset\n")
-        file.write(b"   FldUnits(1) = ,\n")
-        file.write(b"   FldName(1) = \"Depth\",\n")
-        file.write(b"/\n")
-        # IDD
-        file.write(b"IDD 1\n")
-        np.savetxt(file, bathy, fmt='%.5f', newline=os.linesep)
-        file.close()
-        return mydir
+    def run_model(self,bathy,idx=0):
 
-    def read_output(self, mydir):
-        
-        # read outputs from mydir
-        try: 
-        # read significant wave height, mean period, mean direction from "stwave_out.wave.out" 
-            with open(os.path.join(mydir,'stwave_out.wave.out'),'r') as f:# wave height, mean wave period, mean wave direction
-                # DataDims
-                for line in f:
-                    if line.rstrip() == "/":
-                        break
+        sim_dir = self.create_dir(idx)
+        self.stwp = STWaveProblem(nx=self.nx, ny=self.ny, Lx=self.Lx, Ly=self.Ly,
+                                  x0=self.x0, y0=self.y0, t1=self.t1, t2=self.t2,
+                                  offline_dataloc=self.offline_dataloc,
+                                  outputdir=sim_dir, parallel=self.parallel,
+                                  testname='stwave_out')
+        self.stwp.setup()
+        self.stwp.bathy_true = bathy
+        self.stwp.run()
+        # stwp.write_grid_coordinates_to_file()
 
-                # DataSets
-                for line in f:
-                    if line.rstrip() == "/":
-                        break
+        simul_obs = read_speed(sim_dir, 'stwave_out.sim')
 
-                # read IDD
-                f.next()
-        
-                #IDD
-                waveinfo = [[float(x) for x in line.split()] for line in f]
-                f.close()
-        
-                waveinfo = np.array(waveinfo,'d')
+        if self.deletedir:
+            rmtree(sim_dir, ignore_errors=True)
+            # self.cleanup(sim_dir)
 
-                #Hm0, T, alpha
-        except IOError:
-            print("Could not read file ./stwave_out.wave.out") 
-
-        # read peak wave period from "stwave_out.TP.out" 
-        try:
-            with open(os.path.join(mydir,'stwave_out.Tp.out'),'r') as f: # wave
-                # DataDims
-                for line in f:
-                    if line.rstrip() == "/":
-                        break
-
-                # DataSets
-                for line in f:
-                    if line.rstrip() == "/":
-                        break
-
-                # read IDD
-                f.next()
-        
-                # peak wave preiod
-                Tp = [[float(x) for x in line.split()] for line in f]
-        
-                f.close()
-                Tp = np.array(Tp,'d')
-                # wave speed
-                w = 2.*np.pi/Tp
-        
-        except IOError:
-            print("Could not read file ./stwave_out.Tp.out") 
-
-
-        # Read wave number from c2shore.out
-        try:
-            with open(os.path.join(mydir,'c2shore.out'),'r') as f: # wave number
-                # DataDims
-                for line in f:
-                    if line.rstrip() == "/":
-                        break
-
-                # DataSets
-                for line in f:
-                    if line.rstrip() == "/":
-                        break
-        
-                # read IDD
-                f.next()
-        
-                # peak wave preiod
-                k = [[float(x) for x in line.split()] for line in f]
-                f.close()
-                k = np.array(k,'d')
-                with np.errstate(divide='ignore'):
-                    wlen = np.divide(1.,k)
-                wlen[k == 0.] = 0.
-                c = wlen*w # wave speed = freq* length
-
-                # clean generated input/output files
-                if self.deletedir == True:
-                    rmtree(mydir)
-                return c
-
-        except IOError:
-            print("Could not read file ./c2shore.out") 
-
-    def run_model(self,simul_dir):
-        '''
-            run stwave by changing directory to simul_dir, execute, then come back to home directory
-        '''
-        os.chdir(simul_dir)
-        call(["./stwave","stwave_out.sim"])
-        os.chdir(self.homedir)        
-        
-        # or you can run in shell environment
-        #call(["cd " + simul_dir + ";./stwave stwave_out.sim; cd " + self.homedir], shell=True)
-        
-    def run(self,bathy,parallelization,ncores=None):
-        '''
-            write inputs, run stwave, read outputs and remove directory if needed
-        '''
-        nruns = np.size(bathy,1)
-
-        if parallelization:
-            if ncores is None:
-                from psutil import cpu_count # physcial cpu counts
-                ncores = cpu_count(logical=False)
-
-            from joblib import Parallel, delayed  
-
-            # serial reading/writing (may need parallelize this part, but may be ok for now)
-            start = time()
-            myexcutables = []
-            mydirs = []
-            for idx in range(nruns):
-                mydir = self.write_input(bathy[:,idx:idx+1],idx)
-                mydirs.append(mydir)
-                myexcutables.append(os.path.join(mydir,"stwave"))
-        
-            print('-- time for writing stwave input files (sequential) is %g sec' % round(time() - start))
-
-            start = time()
-        
-            # I don't think running in shell mode is recommended but it works for now.
-            Parallel(n_jobs = ncores)(delayed(call)(["cd " + mydirs[idx] + ";./stwave stwave_out.sim"],shell=True) for idx in range(nruns))
-        
-            print('-- time for running %d stwave simulations (parallel) on %d cores is %g sec' % (nruns, ncores,round(time() - start)))
-        
-            start = time()
-        
-            for idx in range(nruns):
-                if idx == 0:
-                    simul_obs = self.read_output(mydirs[idx])
-                else:            
-                    simul_obs = np.concatenate((simul_obs, self.read_output(mydirs[idx])), axis=1)
-        
-            print('-- time for reading stwave output files (sequentially) is %g sec' % round(time() - start))
-        
-            assert(np.size(simul_obs,1) == nruns) # should satisfy this
-
-        else:
-            mydir = self.write_input(bathy,0)
-            self.run_model(mydir)
-            simul_obs = self.read_output(mydir)
-        
         return simul_obs
+
+    def run(self,bathy,par,ncores=None):
+        if ncores is None:
+            ncores = self.ncores
+
+        method_args = range(bathy.shape[1])
+        args_map = [(bathy[:, arg:arg + 1], arg) for arg in method_args]
+
+        if par:
+            pool = Pool(processes=ncores)
+            simul_obs = pool.map(self, args_map)
+        else:
+            simul_obs =[]
+            for item in args_map:
+                simul_obs.append(self(item))
+
+        return np.array(simul_obs).T
+
+        #pool.close()
+        #pool.join()
+
+    def __call__(self,args):
+        return self.run_model(args[0],args[1])
 
 
 if __name__ == '__main__':
     import stwave as st
     import numpy as np
     from time import time
-    from scipy.io import savemat, loadmat
-    bathyfile = loadmat('true_depth.mat')
-    bathy = bathyfile['true']
+
+    bathy = np.loadtxt("true_depth.txt")
+    bathy = bathy.reshape(-1, 1)
     par = False # parallelization false
 
-    mymodel = st.Model()
-    print('1) single run')
+    nx = 110
+    ny = 83
+    Lx = 550
+    Ly = 415
+    x0, y0 = (62.0, 568.0)
+    t1 = dt.datetime(2015, 10, 07, 20, 00)
+    t2 = dt.datetime(2015, 10, 07, 21, 00)
+
+    params = {'nx':nx,'ny':ny,'Lx':Lx,'Ly':Ly,'x0':x0,'y0':y0,'t1':t1,'t2':t2,
+              'offline_dataloc':"./input_files/8m-array_2015100718_2015100722.nc"}
+
+    mymodel = st.Model(params)
+    print('(1) single run')
 
     simul_obs = mymodel.run(bathy,par)
     #savemat('simul.mat',{'simul_obs':simul_obs})    
-    ncores = 12
-    nrelzs = 36
+    ncores = 2
+    nrelzs = 2
     
-    print('2) parallel run with ncores = %d' % ncores)
+    print('(2) parallel run with ncores = %d' % ncores)
     par = True # parallelization false
     bathyrelz = np.zeros((np.size(bathy,0),nrelzs),'d')
     for i in range(nrelzs):
@@ -260,8 +179,6 @@ if __name__ == '__main__':
     print(simul_obs_all)
 
     # use all the physcal cores if not specify ncores
-    print('3) parallel run with all the physical cores')
-    simul_obs_all = mymodel.run(bathyrelz,par)
-
-    print(simul_obs_all)
-
+    #print('(3) parallel run with all the physical cores')
+    #simul_obs_all = mymodel.run(bathyrelz,par)
+    #print(simul_obs_all)
