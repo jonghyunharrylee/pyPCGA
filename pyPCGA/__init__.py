a6dd63548aeea549654c3fe0ae7927835252c0cb
Antoine COLLET <antoine.collet@orano.group>
2021-12-03 18:45:17 +0100

    FIX: typo in __init__.py.

--- pyPCGA/__init__.py
+++ pyPCGA/__init__.py
@@ -1,7 +1,7 @@
 """
 The pyPCGA package enables you to run matrix-free geostatistical inversion
 """
-from __about__ import __version__, __name__, __author__
+from .__about__ import __version__, __name__, __author__
 from .pcga import PCGA
 from . import covariance
 
